69288085f1eaf0bc3cd5272475c3798dad522a4e
Sam <freestff0077@gmail.com>
2025-08-26 01:25:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,8 @@ FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Heisenberg_Universe")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "https://telegra.ph/Extractor-Bot-08-25")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/Extractor-Bot-08-25")
+START_PIC = os.environ.get("START_PIC", "https://envs.sh/tjX.jpg/HGBOTZ.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/tjX.jpg/HGBOTZ.jpg")
 #--------------------------------------------
 
 #--------------------------------------------
